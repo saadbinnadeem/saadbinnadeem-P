210b7ef239137ed2ef767b8ca8a6f207bf8f363e
saadbinnadeem <saadbinnadeem6@gmail.com>
2023-10-09 15:39:42 +0500

    modify project section

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,5 +1,4 @@
 import { Container } from "./styles";
-import githubIcon from "../../assets/github.svg"
 import DownloadApp from '../../assets/download.png'
 import externalLink from "../../assets/external-link.svg"
 import ScrollAnimation from "react-animate-on-scroll";
@@ -8,7 +7,7 @@ import ScrollAnimation from "react-animate-on-scroll";
 export function Project() {
   return (
     <Container id="project">
-      <h2>My Projects</h2>
+      <h2>Project Contributions</h2>
       <div className="projects">
 
   
